f1a68a09656235e42fa045f595eca4b5c6f613bd
lincmill607 <lincmill607@SDS-L-262Q0R>
2025-09-26 11:00:46 -0400

    Add project files.

---
+++ pizzaparlour/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace pizzaparlour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.slicesLabel = new System.Windows.Forms.Label();
+            this.slicesInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subTotalOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Maroon;
+            this.titleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(-4, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(319, 75);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Pizza Parlour";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // slicesLabel
+            // 
+            this.slicesLabel.AutoSize = true;
+            this.slicesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.slicesLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.slicesLabel.Location = new System.Drawing.Point(12, 90);
+            this.slicesLabel.Name = "slicesLabel";
+            this.slicesLabel.Size = new System.Drawing.Size(162, 22);
+            this.slicesLabel.TabIndex = 1;
+            this.slicesLabel.Text = "Number of Slices";
+            // 
+            // slicesInput
+            // 
+            this.slicesInput.Location = new System.Drawing.Point(202, 90);
+            this.slicesInput.Name = "slicesInput";
+            this.slicesInput.Size = new System.Drawing.Size(82, 22);
+            this.slicesInput.TabIndex = 2;
+            this.slicesInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.AutoSize = true;
+            this.calculateButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.calculateButton.Location = new System.Drawing.Point(103, 137);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(103, 38);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate!";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(13, 199);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(70, 18);
+            this.subtotalLabel.TabIndex = 4;
+            this.subtotalLabel.Text = "Subtotal";
+            this.subtotalLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(13, 257);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(97, 18);
+            this.taxLabel.TabIndex = 5;
+            this.taxLabel.Text = "Tax Amount";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(13, 317);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(90, 18);
+            this.totalLabel.TabIndex = 6;
+            this.totalLabel.Text = "Total Price";
+            // 
+            // subTotalOutput
+            // 
+            this.subTotalOutput.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.subTotalOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.subTotalOutput.Location = new System.Drawing.Point(200, 199);
+            this.subTotalOutput.Name = "subTotalOutput";
+            this.subTotalOutput.Size = new System.Drawing.Size(84, 23);
+            this.subTotalOutput.TabIndex = 7;
+            this.subTotalOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.taxOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.taxOutput.Location = new System.Drawing.Point(200, 252);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(84, 23);
+            this.taxOutput.TabIndex = 8;
+            this.taxOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.totalOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.totalOutput.Location = new System.Drawing.Point(200, 312);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(84, 23);
+            this.totalOutput.TabIndex = 9;
+            this.totalOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(312, 373);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.subTotalOutput);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.slicesInput);
+            this.Controls.Add(this.slicesLabel);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label slicesLabel;
+        private System.Windows.Forms.TextBox slicesInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subTotalOutput;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalOutput;
+    }
+}
+
---
+++ pizzaparlour/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pizzaparlour
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        double slicePrice = 3.25;
+        int numOfSlices = 0;
+        double taxRate = 0.13;
+        double subtotal = 0;
+        double taxAmount = 0;
+        double totalPrice = 0;
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numOfSlices = Convert.ToInt16(slicesInput.Text);
+            subtotal = numOfSlices * slicePrice;
+            subTotalOutput.Text = subtotal.ToString("C");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
